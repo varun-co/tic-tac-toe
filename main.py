8b5198813a8c3f34c3f3f027d46e9c254cfe7d13
Varun <varungiri13@gmail.com>
2021-11-17 00:46:15 +0530

    Fancy dispaly bug fixed

--- main.py
+++ main.py
@@ -1,23 +1,28 @@
+side_border = '|'
+border_symbol = '-'
 def create_tic_tac_toe_board(n):
     # will return an a list of list containing all the elements in the borad
     tic_tac_toe = []
     for i in range(n):
         tic_tac_toe.append([])
         for j in range(n):
-            tic_tac_toe[i].append('#')
+            tic_tac_toe[i].append('-')
     return tic_tac_toe
 def display(tic_tac_toe):
     for i in tic_tac_toe:
         print(*i,sep='    ')
 def fancy_display(tic_tac_toe):
-    for i in range(3*len(tic_tac_toe)+2):
-        print('_',end='')
+    for i in range(3*len(tic_tac_toe)+4):
+        print(border_symbol,end='')
     print()
     for i in tic_tac_toe:
-        print('-',*i,sep='   ','_')
-    for i in range(3*len(tic_tac_toe)+2):
-        print('_',end='')
-    print()
+        print(side_border + ' ',end='')
+        print(*i,sep= ' '  +side_border  + ' ',end=' ')
+        print(side_border+' ')
+        for i in range(3*len(tic_tac_toe)+4):
+            print(border_symbol,end='')
+        print()
+
 
 def main():
     tic_tac_toe = create_tic_tac_toe_board(3)
